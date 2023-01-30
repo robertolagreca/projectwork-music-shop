25c78e6fee5dd55527cdf6585531b22d69ee8251
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-30 12:36:58 +0100

    Creato e completato InstrumentApiController

---
+++ ShopStrumentiMusicali/Controllers/InstrumentApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopStrumentiMusicali.Controllers {
+    public class InstrumentApiController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
