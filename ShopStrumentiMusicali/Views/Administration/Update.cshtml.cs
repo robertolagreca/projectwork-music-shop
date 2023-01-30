bd697755ced35724f530eb18e28ec92ff84869d1
robertolagreca <robertolagreca92@gmail.com>
2023-01-30 11:02:58 +0100

    Aggiunta view update per amminstrazione.

---
+++ ShopStrumentiMusicali/Views/Administration/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopStrumentiMusicali.Views.Administration
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
