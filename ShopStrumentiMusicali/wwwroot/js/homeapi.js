2c2f7ac84eac110cb69d227c51587b34b9e6c443
robertolagreca <robertolagreca92@gmail.com>
2023-01-30 15:28:58 +0100

    Aggiunta api get per index

--- ShopStrumentiMusicali/wwwroot/js/homeapi.js
+++ ShopStrumentiMusicali/wwwroot/js/homeapi.js
@@ -1,7 +1,35 @@
 //AVVISO ANCORA IN BOZZA. DA CONTROLLARE NOMI VARIABILI.
 
-//API per la searchbar
 
+//Per visualizzare per ogni prodotto su index
+
+//AVVISO ANCORA IN BOZZA. DA CONTROLLARE NOMI VARIABILI.
+const id = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+axios.get("/api/NOME CONTROLLER API/" + id).then((res) => {
+    let instrument = res.data;
+    console.log('strumento visualizzato', instrument);
+
+
+    document.getElementById('instrument_container').innerHTML +=
+        `
+                        <div class="col-12 col-md-4 p-2">
+                              <div class="card post h-100">
+                                    <img src="${instrument.imageUrl}" class="card-img-top" alt="strumento">
+                                    <div class="card-body">
+                                    <h5 class="card-title">${instrument.name}</h5>
+                                            <p class="card-text">${instrument.description}</p>
+                                            <p class="card-text">${instrument.price}</p>
+                                    </div>
+                            </div>
+                        </div>
+                    `;
+
+});
+
+// LE ALTRI DUE API NON ANCORA ATTIVE.
+
+//API per la searchbar
+/*
 document.getElementById('input_search').addEventListener('keyup', searchInstruments);
 
 function searchInstruments() {
@@ -87,3 +115,4 @@ function sendInstrument(e){
     });
 
     }
+    */
